7f2a3a59a91cb9a38cf1b4cded9bdbd9c1b90d97
alvin.yeoh <alvin.yeoh@gumi.sg>
2021-07-04 13:39:20 +0800

    Added content-type check

--- server/middleware.go
+++ server/middleware.go
@@ -10,6 +10,12 @@ import (
 func checkAPIKey(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
 
+		if req.Header.Get("Content-type") != "application/json" {
+			var jsonResponse models.JSONResponse
+			jsonResponse.PrintJSONResponse(res, http.StatusUnsupportedMediaType, "wrong content-type", nil)
+			return
+		}
+
 		apikey := strings.TrimSpace(req.FormValue("apikey"))
 
 		if apikey != os.Getenv("API_KEY") {
